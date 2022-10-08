c751a5276aa7b79eb754a00fb2e1448d680c52a7
alikhalili66 <Khalili@DESKTOP-AT89HOJ>
2022-10-08 12:53:51 +0330

    build BD20221008_0_0_0_0_0_2

--- odds-admin-services/src/main/java/ir/khalili/products/odds/core/verticle/group/VRTCL_09_GroupCompetitionFetch.java
+++ odds-admin-services/src/main/java/ir/khalili/products/odds/core/verticle/group/VRTCL_09_GroupCompetitionFetch.java
@@ -39,7 +39,7 @@ public class VRTCL_09_GroupCompetitionFetch extends AbstractVerticle {
 		    		
 		    		SQLConnection sqlConnection = connection.result();
 
-					Biz_09_GroupCompetitionFetch.groupFetch(sqlConnection, (JsonObject)(message.body()), resultHandler -> {
+					Biz_09_GroupCompetitionFetch.groupFetch(vertx, sqlConnection, (JsonObject)(message.body()), resultHandler -> {
 	
 						if (resultHandler.succeeded()) {
 							logger.trace("AVTCL08,Succeeded:"+resultHandler.result());
